2ac4731748ef6fac1f85c5954541a697b2d25849
naomiruth <nsmith22@mail.bbk.ac.uk>
2017-03-02 18:55:05 +0000

    various edits
    Added text to submissions and index pages. Edited nav bar links. Edited
    jquery to tidy up the subscribe button.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('div').mouseenter(function() {
-      $('div').fadeTo('fast', 1);
+      $('div').fadeTo('fast', 0.5);
   });
   $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 0.5);
+        $('div').fadeTo('fast', 1);
   });
 });
