c07ef4f3cd7f7977efdaab21c8b46b6cb16adace
naomiruth <nsmith22@mail.bbk.ac.uk>
2017-03-05 17:33:32 +0000

    various changes
    added javascript, images, header and extra text to index.html.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-  $('div').mouseenter(function() {
-      $('div').fadeTo('fast', 0.5);
+$("button").on("click", function() {
+    alert("Subscribed!")
   });
-  $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 1);
-  });
-});
