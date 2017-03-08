08bf28373f6501f62cd33a60e9fbb1a137e58f25
naomiruth <nsmith22@mail.bbk.ac.uk>
2017-03-08 13:03:59 +0000

    form and jquery
    added form to contact page. added submissions button on index page using
    jquery

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-$("")
+$(document).ready(function() {
+    $('input').mouseenter(function() {
+        $('div').fadeTo('fast', 0.5);
+    });
+    $('div').mouseleave(function() {
+        $('div').fadeTo('fast', 1);
+    });
+});
